3759f225f5ffa5065c99a1e4a3dc02b3e4efc415
Jacquese <jacquesewilson@gmail.com>
2021-05-31 20:24:25 -0400

    CRUD and Model creation completed.

--- Revature/main/dao/User/UserDAO.java
+++ Revature/main/dao/User/UserDAO.java
@@ -2,18 +2,23 @@ package main.dao.User;
 
 import main.models.User;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDAO {
 
+  //CREATE USER
   void insertUser(User user) throws SQLException;
-  boolean updateUser(User user) throws SQLException;
-  void selectUser(String username) throws SQLException;
+
+  ///READ DATABASE
+  List<User> selectUser(String username) throws SQLException;
   List<User> selectAllUsers() throws SQLException;
+  List<User> iterateDB(ResultSet resultSet) throws SQLException;
 
-  User selectUser(User user) throws SQLException;
+  //UPDATE FULL ROW
+  List<User> updateUser(User user) throws SQLException;
 
-  boolean deleteUser(int id) throws SQLException;
-  boolean deleteUser(String username);
+  //DELETE BASED ON ID
+  List<User> deleteUser(int id) throws SQLException;
 }
--- Revature/main/dao/User/UserDAOImpl.java
+++ Revature/main/dao/User/UserDAOImpl.java
@@ -1,7 +1,6 @@
 package main.dao.User;
 
 import main.models.User;
-import main.models.Users.Admin;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -21,7 +20,6 @@ public class UserDAOImpl implements UserDAO {
   private String usernam, nam, passwor, role;
   private int[] accts;
   private static List<User> users=new ArrayList<User>();
-
   public Connection getConnection(String dbName) throws SQLException {
     try {
       PropertyConfigurator.configure("Revature/resources/log4jtest.properties");
@@ -51,82 +49,153 @@ public class UserDAOImpl implements UserDAO {
     return DriverManager.getConnection(jdbcUrl);
   }
 
-  private void teststuff(){
-    User Bob= new User("Bob1", "pwd1234");
-    User Jock=new Admin("Jock1","wellya","Admin Rogers!");
-    // User Bob= new User("Bob6", "pwd1234");
-    //users.add(Bob);
-    //dao.insertUser(Bob);
-    //dao.selectAllUsers();
-    //users.get(0).setName("OG Bobby");
-    //dao.updateUser(users.get(0));
-    //dao.selectAllUsers();
-    users.add(Bob);
-  }
   public UserDAOImpl(){ }
 
   public static void main(String[] args) throws SQLException {
     UserDAOImpl dao = new UserDAOImpl();
-    //dao.selectAllUsers();
-    dao.deleteUser(1269539269);
+    //System.out.println( dao.selectUser("Bob"));
+    System.out.println(dao.selectAllUsers());
   }
 
   ///CREATE USERS
   @Override
   public void insertUser(User user) throws SQLException {
-    Statement stmt=null;
     try {
       PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
     } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
     logger.setLevel(Level.WARN);
     Connection connection=getConnection("eqbank");
-    insert=("INSERT INTO users (userid, username,NAME,password,account1, accounts, role) values ("+user.getUserid()+",'"+user.getUsername()+"','"+user.getName()+"', '"+user.getPassword()+"',"+null+","+ Arrays.toString(user.getAccountz()) +", '"+user.getRole()+"');");
-    //Run SQL Query
+    Statement stmt=null;
+
     try {
+      insert=("INSERT INTO users (userid, username,NAME,password,account1, accounts, role) values ("+user.getUserid()
+        +",'"+user.getUsername() +"','"+user.getName()+"', '"+user.getPassword()+"',"+ Arrays.toString(user.getAccounts()) +","+
+        Arrays.toString(user.getAccounts()) +", '"+user.getRole()+"');");
+
       connection= getConnection("eqbank");
       stmt=connection.createStatement();
-      int result=stmt.executeUpdate(insert);
-      System.out.println(result); } catch (Exception e){e.printStackTrace();}
+      int result=stmt.executeUpdate(insert);System.out.println(result); }
+    catch (Exception e){e.printStackTrace();}
+    finally {
+      assert stmt != null;
+      stmt.close();connection.close(); }
+    //logger.info("New User added successfully:\n"+user.printUsers());
+  }
+
+  ///READ DATABASE
+  @Override
+  public List<User> selectUser(String user) throws SQLException {
+    try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties"); }
+    catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;} logger.setLevel(Level.WARN);
+
+    List<User> instList=new ArrayList<User>();
+    Connection connection=getConnection("eqbank");
+    Statement stmt=null;
+    try{
+      sql="SELECT * FROM USERS WHERE username='"+user+"';";
+      stmt=connection.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+      ResultSet rs=stmt.executeQuery(sql);
+      instList=null;
+      instList=iterateDB(rs); }
+    catch (Exception e){e.printStackTrace();}
+    finally {
+      assert stmt != null;
+      stmt.close();
+      connection.close(); }
+    logger.info("Success");
+    count=0;
+    return instList;
+  }
+  @Override
+  public List<User> selectAllUsers() throws SQLException {
+    List<User> instList=new ArrayList<User>();
+    Statement stmt=null;
+    Connection connection=getConnection("eqbank");
+    try {
+      PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
+    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
+    //Run SQL Query
+    try {
+      connection=getConnection("eqbank");
+      selectall="SELECT * FROM USERS";
+      stmt=connection.createStatement();
+      ResultSet rs=stmt.executeQuery(selectall);
+      instList=null;
+      instList=iterateDB(rs); }
+    catch (Exception e){e.printStackTrace();}
+    finally {
+      assert stmt != null;
+      stmt.close();
+      connection.close(); }
+    logger.info("Success");
+    count=0;
+    return instList;
+  }
+
+  ///UPDATE DATABASE
+  @Override
+  public List<User> updateUser(User user) throws SQLException {
+    try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
+    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;} logger.setLevel(Level.WARN);
+    List<User> instList=new ArrayList<User>();
+    Connection connection=getConnection("eqbank");
+    Statement stmt=null;
+
+    try {
+      update="update users SET userid="+ user.getUserid()+", username='"+user.getUsername()+"', name='"+user.getName()+"', password='"+user.getPassword()+"',account1="+null+", accounts="+ Arrays.toString(user.getAccounts()) +",role= '"+user.getRole()+"' WHERE userid="+user.getUserid()+";";
+      instList=null;
+      connection= getConnection("eqbank");
+      stmt=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      connection.setAutoCommit(false);
+      stmt.addBatch(update);
+      stmt.executeBatch();
+      connection.commit();
+      instList=selectAllUsers(); }
+    catch (Exception e){e.printStackTrace();}
     finally {
       assert stmt != null;
       stmt.close();
       connection.close(); }
 
-    logger.info("New User added successfully:\n"+user.printUsers());
+   // logger.info("New User updated successfully:\n"+user.printUsers());
+    return instList;
   }
 
-  ///READ DATABASE
-  @Override
-  public void selectUser(String username) throws SQLException {
-    Statement stmt=null;
-    try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
-    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
-    logger.setLevel(Level.WARN);
-    Connection connection=getConnection("eqbank");
+ ///DELETE USER FROM DATABASE
+ @Override
+ public List<User> deleteUser(int id) throws SQLException {
+   try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
+   } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}logger.setLevel(Level.WARN);
 
-    selectall="SELECT * FROM USERS WHERE username='"+username+"';";
-    try{
-    stmt=connection.createStatement();
-    ResultSet rs=stmt.executeQuery(selectall);
-    if(rs.next()){
-      while (rs.next()){
-        id = rs.getInt("userid");
-        usernam = rs.getString("username");
-        nam = rs.getString("name");
-        passwor = rs.getString("password");
-        acc1= rs.getInt("account1");
-        accts = (int[]) rs.getObject("accounts");
-        role=rs.getString("role");
-        System.out.println("--------------"+count+"--------------");
-        System.out.println( "ID = " + id );
-        System.out.println( "USERNAME = " + usernam );
-        System.out.println( "NAME = " + nam );
-        System.out.println( "PASSWORD = " + passwor );
-        System.out.println( "MAIN ACCOUNT = " +acc1 );
-        System.out.println( "ACCOUNTS = " + Arrays.toString(accts));
-        System.out.println( "ROLE = " + role );
-        count++;
-      } } else{
+   List<User> instList=new ArrayList<User>();
+   Connection connection=getConnection("eqbank");
+   Statement stmt=null;
+   try {
+     update="DELETE FROM users WHERE userid="+id+";";
+     selectall="SELECT * FROM USERS;";
+     instList=null;
+     connection= getConnection("eqbank");
+     stmt=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+     connection.setAutoCommit(false);
+     stmt.addBatch(update);
+     stmt.executeBatch();
+     connection.commit();
+     instList=selectAllUsers();
+   } catch (Exception e){e.printStackTrace();}
+   finally {
+     assert stmt != null;
+     stmt.close();
+     connection.close(); }
+     //logger.info("New User updated successfully:\n"+use.printUsers());
+   return instList;
+ }
+
+ @Override
+ public List<User> iterateDB(ResultSet rs) throws SQLException {
+    User instance = new User();
+    List<User> instList=new ArrayList<User>();
+
+    while(rs.next()){
       id = rs.getInt("userid");
       usernam = rs.getString("username");
       nam = rs.getString("name");
@@ -134,6 +203,13 @@ public class UserDAOImpl implements UserDAO {
       acc1= rs.getInt("account1");
       accts = (int[]) rs.getObject("accounts");
       role=rs.getString("role");
+      instance.setUserid(id);
+      instance.setUsername(usernam);
+      instance.setName(nam);
+      instance.setPassword(passwor);
+      instance.setAccounts(accts);
+      instance.setRole(role);
+      instList.add(instance);
       System.out.println("--------------"+count+"--------------");
       System.out.println( "ID = " + id );
       System.out.println( "USERNAME = " + usernam );
@@ -142,155 +218,38 @@ public class UserDAOImpl implements UserDAO {
       System.out.println( "MAIN ACCOUNT = " +acc1 );
       System.out.println( "ACCOUNTS = " + Arrays.toString(accts));
       System.out.println( "ROLE = " + role );
-    }} catch (Exception e){e.printStackTrace();}
-    finally {
-    assert stmt != null;
-    stmt.close();
-    connection.close(); }
-    logger.info("Success");
-  count=0;
+      count++;}
+
+    return instList;
   }
-  @Override
-  public List<User> selectAllUsers() throws SQLException {
-    Statement stmt=null;
-    Connection connection=getConnection("eqbank");
-    try {
-      PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
-    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
-    //Run SQL Query
-    try {
-      connection=getConnection("eqbank");
-      selectall="SELECT * FROM USERS";
-      stmt=connection.createStatement();
-      ResultSet rs=stmt.executeQuery(selectall);
+  protected void CheckTable(ResultSet rs) throws SQLException {
+    ResultSetMetaData metadata = rs.getMetaData();
+    int columnCount = metadata.getColumnCount();
+    for (int i = 1; i <= columnCount; i++) {
+      System.out.println(metadata.getColumnName(i) + ", ");
+    }
+    System.out.println();
+    while (rs.next()) {
+      String row = "";
+      for (int i = 1; i <= columnCount; i++) {
+        row += rs.getString(i) + ", ";
+      }
+      System.out.println();
+      System.out.println(row);
 
-      while(rs.next()){
-        id = rs.getInt("userid");
-        usernam = rs.getString("username");
-        nam = rs.getString("name");
-        passwor = rs.getString("password");
-        acc1= rs.getInt("account1");
-        accts = (int[]) rs.getObject("accounts");
-        role=rs.getString("role");
-        System.out.println("--------------"+count+"--------------");
-        System.out.println( "ID = " + id );
-        System.out.println( "USERNAME = " + usernam );
-        System.out.println( "NAME = " + nam );
-        System.out.println( "PASSWORD = " + passwor );
-        System.out.println( "MAIN ACCOUNT = " +acc1 );
-        System.out.println( "ACCOUNTS = " + Arrays.toString(accts));
-        System.out.println( "ROLE = " + role );
-        count++;
-      } } catch (Exception e){e.printStackTrace();}
-    finally {
-      assert stmt != null;
-      stmt.close();
-      connection.close(); }
-    logger.info("Success");
-    count=0;
-    return null;
+    }
   }
-
-  ///UPDATE DATABASE
-  @Override
-  public boolean updateUser(User user) throws SQLException {
-    //set new value then send here
-    Statement stmt=null;
-    try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
-    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
-    logger.setLevel(Level.WARN);
-    Connection connection=getConnection("eqbank");
-    System.out.println(user.getUserid());
-    update="update users SET userid="+ user.getUserid()+", username='"+user.getUsername()+"', name='"+user.getName()+"', password='"+user.getPassword()+"',account1="+null+", accounts="+ Arrays.toString(user.getAccountz()) +",role= '"+user.getRole()+"' WHERE userid="+user.getUserid()+";";
-    //Run SQL Query
-    try {
-      connection= getConnection("eqbank");
-      stmt=connection.createStatement();
-      int result = stmt.executeUpdate(update);
-      System.out.println(result); } catch (Exception e){e.printStackTrace(); return false;}
-    finally {
-      assert stmt != null;
-      stmt.close();
-      connection.close(); }
-
-    logger.info("New User updated successfully:\n"+user.printUsers());
-    return true;
-  }
-
- ///DELETE USER FROM DATABASE
- @Override
- public boolean deleteUser(int id) throws SQLException {
-   Statement stmt=null;
-   try { PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
-   } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
-   logger.setLevel(Level.WARN);
-   Connection connection=getConnection("eqbank");
-   update="DELETE FROM users WHERE userid="+id+";";
-   selectall="SELECT * FROM USERS;";
-   try {
-     connection= getConnection("eqbank");
-     stmt=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-     connection.setAutoCommit(false);
-     stmt.addBatch(update);
-     stmt.executeBatch();
-     connection.commit();
-     selectAllUsers();
-   } catch (Exception e){e.printStackTrace(); return false;}
-   finally {
-     assert stmt != null;
-     stmt.close();
-     connection.close(); }
-
-     //logger.info("New User updated successfully:\n"+use.printUsers());
-   return false;
- }
-
- //Return User Object
-  @Override
-  public User selectUser(User user) throws SQLException {
-    Statement stmt=null;
-    Connection connection=getConnection("eqbank");
-    try {
-      PropertyConfigurator.configure("Revature/resources/properties/log4j.properties");
-    } catch (Exception e){e.printStackTrace(); System.out.println("Seems not found");;}
-    logger.setLevel(Level.WARN);
-    //Run SQL Query
-    try {
-      connection=getConnection("eqbank");
-      selectall="SELECT * FROM USERS";
-      stmt=connection.createStatement();
-      ResultSet rs=stmt.executeQuery(selectall);
-
-      while(rs.next()){
-        id = rs.getInt("userid");
-        usernam = rs.getString("username");
-        nam = rs.getString("name");
-        passwor = rs.getString("password");
-        acc1= rs.getInt("account1");
-        accts = (int[]) rs.getObject("accounts");
-        role=rs.getString("role");
-        System.out.println("--------------"+count+"--------------");
-        System.out.println( "ID = " + id );
-        System.out.println( "USERNAME = " + usernam );
-        System.out.println( "NAME = " + nam );
-        System.out.println( "PASSWORD = " + passwor );
-        System.out.println( "MAIN ACCOUNT = " +acc1 );
-        System.out.println( "ACCOUNTS = " + Arrays.toString(accts));
-        System.out.println( "ROLE = " + role );
-        count++;
-      } } catch (Exception e){e.printStackTrace();}
-    finally {
-      assert stmt != null;
-      stmt.close();
-      connection.close(); }
-    logger.info("Success");
-    count=0;
-    return null;
-  }
-
-  //Return remaining Users as List<User>
-  @Override
-  public boolean deleteUser(String username) {
-    return false;
+  protected void garage(){
+    //User Bob= new User("Bob1", "pwd1234");
+    // User Jock=new Admin("Jock1","wellya","Admin Rogers!");
+    // User Bob= new User("Bob6", "pwd1234");
+    //users.add(Bob);
+    //dao.insertUser(Bob);
+    //dao.selectAllUsers();
+    //users.get(0).setName("OG Bobby");
+    //dao.updateUser(users.get(0));
+    //dao.selectAllUsers();
+    //users.add(Bob);
   }
 }
+
